dafcb2fed655027919cdd3765efeee7a836f5ca7
ceejay077 <117851139+ceejay077@users.noreply.github.com>
2025-04-20 00:47:33 +0530

    32.05

--- client1/src/App.js
+++ client1/src/App.js
@@ -1,22 +1,30 @@
 import "./App.css";
 import Header from "./header";
+import Layout from "./layout";
 import Post from "./post";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route
-        index
-        element={
-          <main>
-            <Header />
-            <Post />
-            <Post />
-            <Post />
-          </main>
-        }
-      />
+      <Route path="/" element={<Layout />}>
+        <Route
+          index
+          element={
+            <main>
+              <Post />
+            </main>
+          }
+        />
+        <Route
+          path={"/login"}
+          element={
+            <main>
+              <div>login Page</div>
+            </main>
+          }
+        />
+      </Route>
     </Routes>
   );
 }
